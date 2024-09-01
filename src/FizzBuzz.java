c4e75d195cc0fcd2d9f4f578d4f68d8ced21770c
sazuko07 <66929181+sazuko07@users.noreply.github.com>
2024-08-31 20:30:27 -0500

    Update FizzBuzz.java

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -2,9 +2,21 @@
 
 public class FizzBuzz {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		int i;
+		for (i = 1; i <= 100; i = i +1) {
+			
+			
+		
+		if (i % 3 == 0 && i % 5 == 0) {
+			System.out.println("FizzBuzz"); }
+			else if(i % 3 == 0) {
+				System.out.println("Fizz"); }
+			else if(i % 5 == 0) {
+				System.out.println("Buzz"); }
+		
+			else {
+				System.out.println(i);
+			}
+			}
+		}
+}	
